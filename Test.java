83d185446f2a9b4a37e44798a83dbfe7b862ba7a
rahimov27 <89564054+rahimov27@users.noreply.github.com>
2023-06-26 23:40:45 +0200

    absolutely final update 100%

--- Test.java
+++ Test.java
@@ -80,20 +80,20 @@ abstract class Test {
         obj4.printItem();
 
 //        //bestShipping method
-//        objects.bestShipping(sumVolume,sumWeight, containerSmall.calcVolumeOfContainer(), containerBig.calcVolumeOfContainer());
-//        System.out.println("---------------------------------------------------------------------");
+        objects.bestShipping(sumVolume,sumWeight, containerSmall.calcVolumeOfContainer(), containerBig.calcVolumeOfContainer());
+        System.out.println("---------------------------------------------------------------------");
 
         //shippingPrice method
         objects.shippingPrice(sumVolume, containerSmall.calcVolumeOfContainer(), containerBig.calcVolumeOfContainer());
         System.out.println("---------------------------------------------------------------------");
 
         //addOrder method
-//        objects.addOrder(sumVolume, containerSmall.calcVolumeOfContainer(), containerBig.calcVolumeOfContainer());
-//        System.out.println("---------------------------------------------------------------------");
-//
-//        //printOrder method
-//        objects.printOrder(sumVolume, containerSmall.calcVolumeOfContainer(), containerBig.calcVolumeOfContainer());
-//        System.out.println("---------------------------------------------------------------------");
+        objects.addOrder(sumVolume, containerSmall.calcVolumeOfContainer(), containerBig.calcVolumeOfContainer());
+        System.out.println("---------------------------------------------------------------------");
+
+        //printOrder method
+        objects.printOrder(sumVolume, containerSmall.calcVolumeOfContainer(), containerBig.calcVolumeOfContainer());
+        System.out.println("---------------------------------------------------------------------");
 
 
     }
